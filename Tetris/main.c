bf07a954bf8e16a8486460485aab301895af6f77
Benjamin Bachmayr <Benjamin Bachmayr@Corsair380t>
2016-11-25 22:08:13 +0100

    creating threads

--- Tetris/main.c
+++ Tetris/main.c
@@ -11,8 +11,28 @@
 /*
  * 
  */
+void* Thread1();
+void* Thread2();
+
 int main(int argc, char** argv) {
+    
+    pthread_t thread1, thread2;
+    
+    pthread_create( &thread1, NULL, Thread1, NULL);
+    pthread_create( &thread2, NULL, Thread2, NULL);
+    
+    
 
     return (EXIT_SUCCESS);
 }
 
+void* Thread1(){
+    while(1){
+        
+    }
+}
+void* Thread2(){
+    while(1){
+        
+    }
+}
