1227684d55f2a0b9592c0da1b6eab2ec14961567
Benjamin Bachmayr <Benjamin Bachmayr@Corsair380t>
2016-11-25 21:57:30 +0100

    creating project

---
+++ Tetris/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Benjamin Bachmayr
+ *
+ * Created on 25. November 2016, 21:57
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
